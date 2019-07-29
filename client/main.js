4501e7881918025b5d93450294e29f17d053fd23
unknown <v.dhanendra@gmail.com>
2019-07-29 21:47:42 +0530

    added photos

--- client/main.js
+++ client/main.js
@@ -6,3 +6,10 @@ import '/imports/client/image_share.css';
 import '/imports/client/image_share.html';
 
 
+// import the code that defines the collections
+import '/imports/api/collections.js';
+// import the main client code (routes, helpers, events etc.)
+// you might want to separate those things into separate files...
+import '/imports/client/main.js';
+
+
