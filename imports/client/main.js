16a32f7f9ade037c97c257498b521317bb4cde3b
unknown <v.dhanendra@gmail.com>
2019-08-20 23:45:05 +0530

    added welcome route

--- imports/client/main.js
+++ imports/client/main.js
@@ -3,3 +3,15 @@ import { Router } from 'meteor/iron:router';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
+/// routing
+
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+  this.render('welcome', {
+    to:"main"
+  });
+});
+
