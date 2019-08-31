d6e37646a79b7a7e18ec4dc58a968b62ae737dc5
unknown <v.dhanendra@gmail.com>
2019-08-31 22:53:37 +0530

    added scroll laoding

--- imports/client/main.js
+++ imports/client/main.js
@@ -35,3 +35,24 @@ Router.route('/image/:_id', function () {
     }
   });
 });
+
+
+/// infiniscroll
+
+Session.set("imageLimit", 8);
+lastScrollTop = 0;
+$(window).scroll(function(event){
+// test if we are near the bottom of the window
+if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+  // where are we in the page?
+  var scrollTop = $(this).scrollTop();
+  // test if we are going down
+  if (scrollTop > lastScrollTop){
+    // yes we are heading down...
+   Session.set("imageLimit", Session.get("imageLimit") + 4);
+  }
+
+  lastScrollTop = scrollTop;
+}
+
+})
