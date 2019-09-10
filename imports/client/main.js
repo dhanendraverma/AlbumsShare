51ab098ee2dddc8949640bdbbd7c80292b60b6eb
unknown <v.dhanendra@gmail.com>
2019-09-10 23:02:30 +0530

    add image template helpers

--- imports/client/main.js
+++ imports/client/main.js
@@ -67,3 +67,42 @@ passwordSignupFields: "USERNAME_AND_EMAIL"
 });
 
 ///
+
+
+Template.images.helpers({
+images:function(){
+  if (Session.get("userFilter")){// they set a filter!
+    return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}});
+  }
+  else {
+    return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});
+  }
+},
+filtering_images:function(){
+  if (Session.get("userFilter")){// they set a filter!
+    return true;
+  }
+  else {
+    return false;
+  }
+},
+getFilterUser:function(){
+  if (Session.get("userFilter")){// they set a filter!
+    var user = Meteor.users.findOne(
+      {_id:Session.get("userFilter")});
+    return user.username;
+  }
+  else {
+    return false;
+  }
+},
+getUser:function(user_id){
+  var user = Meteor.users.findOne({_id:user_id});
+  if (user){
+    return user.username;
+  }
+  else {
+    return "anon";
+  }
+}
+});
