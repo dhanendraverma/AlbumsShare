adb7453c07aa8ca51122b528038a6fbd61e26b80
unknown <v.dhanendra@gmail.com>
2019-09-11 23:48:38 +0530

    add image helper function

--- imports/client/main.js
+++ imports/client/main.js
@@ -106,3 +106,39 @@ getUser:function(user_id){
   }
 }
 });
+
+
+
+Template.images.events({
+'click .js-image':function(event){
+    $(event.target).css("width", "50px");
+},
+'click .js-del-image':function(event){
+   var image_id = this._id;
+   console.log(image_id);
+   // use jquery to hide the image component
+   // then remove it at the end of the animation
+   $("#"+image_id).hide('slow', function(){
+    Images.remove({"_id":image_id});
+   })
+},
+'click .js-rate-image':function(event){
+  var rating = $(event.currentTarget).data("userrating");
+  console.log(rating);
+  var image_id = this.data_id;
+  console.log(image_id);
+
+  Images.update({_id:image_id},
+                {$set: {rating:rating}});
+},
+'click .js-show-image-form':function(event){
+    console.log("showing the modal...");
+  $("#image_add_form").modal('show');
+},
+'click .js-set-image-filter':function(event){
+    Session.set("userFilter", this.createdBy);
+},
+'click .js-unset-image-filter':function(event){
+    Session.set("userFilter", undefined);
+},
+});
