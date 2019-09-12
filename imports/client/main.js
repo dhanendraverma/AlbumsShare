5db2d646778f740a2b94278c449d4b37376decf9
unknown <v.dhanendra@gmail.com>
2019-09-12 23:16:52 +0530

    image add form

--- imports/client/main.js
+++ imports/client/main.js
@@ -142,3 +142,24 @@ Template.images.events({
     Session.set("userFilter", undefined);
 },
 });
+
+
+Template.image_add_form.events({
+'submit .js-add-image':function(event){
+  var img_src, img_alt;
+
+    img_src = event.target.img_src.value;
+    img_alt = event.target.img_alt.value;
+    console.log("src: "+img_src+" alt:"+img_alt);
+    if (Meteor.user()){
+      Images.insert({
+        img_src:img_src,
+        img_alt:img_alt,
+        createdOn:new Date(),
+        createdBy:Meteor.user()._id
+      });
+  }
+    $("#image_add_form").modal('hide');
+ return false;
+}
+});
